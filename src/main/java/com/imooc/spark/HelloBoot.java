1183a08b1ade18e1c2b5ee0b757c40f24d25d01e
hwbaker <hewei860311@sohu.com>
2018-02-25 15:17:03 +0800

    Echarts柱状图饼图-静态

--- src/main/java/com/imooc/spark/HelloBoot.java
+++ src/main/java/com/imooc/spark/HelloBoot.java
@@ -3,6 +3,7 @@ package com.imooc.spark;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * test
@@ -13,5 +14,15 @@ public class HelloBoot {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello() {
         return "Hello World Spring Boot......";
+    }
+
+    @RequestMapping(value = "/first", method = RequestMethod.GET)
+    public ModelAndView firstDemo() {
+        return new ModelAndView("test");
+    }
+
+    @RequestMapping(value = "/course_click_stat", method = RequestMethod.GET)
+    public ModelAndView courseClickStat() {
+        return new ModelAndView("pie");
     }
 }
